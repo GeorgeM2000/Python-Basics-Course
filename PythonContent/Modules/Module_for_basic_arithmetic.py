4bb2feb002af7d1dc1188b939b7d1c9792f0e3f8
George M <giorgos_matlis@hotmail.com>
2021-11-15 21:09:02 +0200

    Create Module_for_basic_arithmetic.py

---
+++ PythonContent/Modules/Module_for_basic_arithmetic.py
@@ -0,0 +1,17 @@
+def addition(x,y):
+    return x+y
+  
+def subtraction(x,y):
+    return x-y
+
+def division(x,y):
+    return x/y
+
+def modulo(x,y):
+    return x%y
+
+
+print(addition(3,5))
+print(subtraction(3,5))
+print(division(5,2))
+print(modulo(10,3))
